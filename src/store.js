3e83882ff97b240580df2e6ce69c6f49b7d049de
PietroMarilei~ <pietromrl0@gmail.com>
2023-10-18 16:12:15 +0200

    store api stored

--- src/store.js
+++ src/store.js
@@ -3,6 +3,11 @@ import { reactive } from "vue";
 
 const store = reactive({
     // 💚 axios in app.vue put all trainers here
+    apiAllTrainers:  'http://127.0.0.1:8000/api/trainers/',
+    apiShowSingleTrainer:'http://127.0.0.1:8000/api/trainer/'+'id',
+    apiAllSpecs: 'http://127.0.0.1:8000/api/specializations/',
+    apiSpecTrainers: '',
+
     allTrainers: '',
 
     allSpecs: []
