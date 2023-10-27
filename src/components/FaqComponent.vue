a9ac70b31f079e5e125221beca2f3791e17faec9
PietroMarilei~ <pietromrl0@gmail.com>
2023-10-27 12:45:40 +0200

    add text from placeholder to faq

--- src/components/FaqComponent.vue
+++ src/components/FaqComponent.vue
@@ -26,49 +26,50 @@ export default {
     <section class="faq">
         <div class="col-12 col-md-6 container-fluid px-3">
             <h2>
-                Domande frequenti
+                Frequently Asked Questions
             </h2>
 
             <hr class="py-3">
             <div class="accordion accordion-flush py-4" id="accordionFlushExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                        Accordion Item #1
-                    </button>
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            How do I enroll in a sports course on the platform?
+                        </button>
                     </h2>
                     <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                        <div class="accordion-body">To enroll, go to the Home page, click "Register," and follow the instructions to complete the registration process.</div>
                     </div>
                 </div>
-                
-            <hr class="py-3">
+
+                <hr class="py-3">
 
                 <div class="accordion-item">
                     <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                        Accordion Item #2
-                    </button>
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            What types of sports courses are available on the platform?
+                        </button>
                     </h2>
                     <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                        <div class="accordion-body">We offer a diverse range, including fitness courses, sport-specific training, group classes, and more. Explore our "Courses" section for the full selection.</div>
                     </div>
                 </div>
-                
-            <hr class="py-3">
+
+                <hr class="py-3">
 
                 <div class="accordion-item">
                     <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                        Accordion Item #3
-                    </button>
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            Is it possible to purchase sponsorships for sports courses on the platform?
+                        </button>
                     </h2>
                     <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                        <div class="accordion-body">Yes, we offer sponsorship opportunities for our sports courses. For details on how to purchase sponsorships and the benefits associated with them, please visit our "Sponsorship Opportunities" section.</div>
                     </div>
                 </div>
-                </div>
+            </div>
             <hr class="py-3">
+          
         </div>
     </section>
 </template>
@@ -79,7 +80,7 @@ export default {
 @use '../assets/scss/variables.scss' as *;
 
 
-.faq{ 
+.faq {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -88,48 +89,52 @@ export default {
     height: 500px;
 
 }
-.small-container{
+
+.small-container {
     margin: 0 auto;
     max-width: 500px;
-  
+
 }
 
 
-.faq h2{
+.faq h2 {
     padding-bottom: 40px;
 }
 
-.faq-section{
+.faq-section {
     position: relative;
     display: flex;
     align-items: center;
-    a .fa-chevron-down{
-    position: absolute;
-    right: 30px;
-    bottom: 30px;
-    font-size: 0.8em;
-}
-p{
-    padding-top: 80px;
-    padding-bottom: 80px;
-}
+
+    a .fa-chevron-down {
+        position: absolute;
+        right: 30px;
+        bottom: 30px;
+        font-size: 0.8em;
+    }
+
+    p {
+        padding-top: 80px;
+        padding-bottom: 80px;
+    }
 }
 
-.accordion-button:not(.collapsed){
+.accordion-button:not(.collapsed) {
     background-color: rgba(232, 124, 93, 1);
     padding: 5px;
     color: white;
 }
+
 .accordion-button:focus {
     border: 1px solid white;
 }
 
-.accordion-button{
+.accordion-button {
     background-color: whitesmoke;
 }
 
-.accordion-item{
-    padding: 20px;
+.accordion-item {
+    // se metti important ti sovrascrive lo stile marcio di Bs 🧡
+    padding: 20px !important;
     background-color: whitesmoke;
-}
-</style>
+}</style>
